64dc2dea4ac585a30f931f89a313dac79af3b7fd
Mihail Sinkov <mihailsin88@gmail.com>
2021-07-22 22:48:23 +0300

    adds mobile styles for header

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,21 @@
     refs.modal.classList.toggle('backdrop--is-hidden');
   }
 })();
+
+
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('dropdown-menu--is-hidden');
+  }
+})();
